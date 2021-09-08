844a8240ac4695a4fda7c2c71fe2d2333352fae0
Alan Brandi <alan.brandi@hotmail.com>
2021-09-08 20:43:00 -0300

    Atualizando o exercicio separando as classes.

---
+++ Calculo_circunferencia_volume_circulo/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculo_circunferencia_volume_circulo
+{
+   public class Calculadora
+   {
+        //Dando valor de PI.
+        public double Pi = 3.14;
+
+        //Calculando Circunferência.
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        //Calculando Volume.
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+   }
+}
